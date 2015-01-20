e6fd6e003067246550d66eebca9cbed72cf4e246
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-20 13:15:40 -0500

    Adding the last part of exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,8 @@
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name} !"
+
+puts "Sooo, #{name}, how old are you??"
+age = gets.chomp
+age2 = 2014 - age.to_i
+puts "Holy frig, you were born in #{age2}?! Just a spring chicken!"
