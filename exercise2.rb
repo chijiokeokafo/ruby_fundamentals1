54fda653fa1f3ea5a7131b24d8030cfd9ec88b7f
Benjamin Alright <benjaminalright@Benjamins-MacBook-Air.local>
2015-01-20 12:59:29 -0500

    Adding exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+
+# Exercise 2 pt. 1
+bill = 55
+tip = bill *0.15
+puts "The bill of #{bill} should tip out #{tip} dollars"
+
+# Exercise 2 pt. 2
+addition = "food!".to_i + 2
+puts addition
+
+# Exercise 2 pt. 3
+
+cash = 45628 * 7839
+puts "I just won the lottery, paid the dealership #{cash} bucks for a new rarri."
+
+# Exercise 2 pt. 4, Which will eval to true
+
+puts (true && false) || (false && true) || !(false && false)
